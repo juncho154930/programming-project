37b98f162d0ae0926aa1b1fe5c652a6f188951fb
juncho154930 <jxc154930@gmail.com>
2023-10-06 15:12:09 -0500

    adding more instructions

--- components/landing/HeroContent.tsx
+++ components/landing/HeroContent.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 
 const HeroContent = () => {
+  const handleClick = () => {
+    alert("Button clicked");
+  };
+
   return (
-    <div>
-      <p className="text-2xl text-center">Write your html/css/js here</p>
+    <div className="w-full h-full">
+      {/* You can remove all of this */}
+      <div className="flex flex-col w-full h-full justify-center items-center">
+        <p className="text-2xl text-center">Write your html/css/js here</p>
+        <button
+          onClick={handleClick}
+          className="rounded border border-black m-8 text-xl"
+        >
+          Test Button
+        </button>
+      </div>
+      {/* You can remove all of this */}
     </div>
   );
 };
